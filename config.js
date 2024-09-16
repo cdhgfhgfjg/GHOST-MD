dc2b4b81b1b502148c50a900441e4137dccd48f3
cdhgfhgfjg <pbmmalkanthi@gmail.com>
2024-09-16 10:16:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NmtHALAZ#EnIFZxsezS449TJBFf4pRjQ-XT-8X9EmE__3noWUPjI",
+SESSION_ID: process.env.SESSION_ID || "0zd10YpT#yu400fn-CDj5BbbdKhv2hS5KNy__DSMukP9GsBWii7Y",
 MONGODB: process.env.MONGODB || "mongodb://mongo:hQnSAQRYDOgXnfEqxmNrWSyJaLVdtvCn@junction.proxy.rlwy.net:59503",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
